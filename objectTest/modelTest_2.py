49dc148dea75cc267de8820f0bf66b8ba077fa78
SUWANKIM <korean.kim@hotmail.com>
2019-01-19 04:41:20 +0900

    #3_ User 클래스 생성

---
+++ objectTest/modelTest_2.py
@@ -0,0 +1,13 @@
+class User:
+    num_users = 0 #class 변수
+    def __init__(self, name):
+        self.name = name
+        User.num_users += 1
+
+
+u = User('honux')
+print(User.num_users, u.name)
+u2 = User('crong')
+print(User.num_users, u2.name)
+print(User.num_users, u.num_users, u2.num_users)
+
